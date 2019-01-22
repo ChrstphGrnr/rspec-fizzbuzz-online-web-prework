8954b760c4e3894d4a47124f5d75eea2e649d550
Christoph Gruener <christophgruener@me.com>
2019-01-22 18:53:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,7 +7,7 @@ def fizzbuzz(number)
     "Fizz"
   elsif number % 5 == 0
     "Buzz"
-  elsif !(number % 3) == 1 || (number % 5) == 1
+  elsif !(number % 3) == 1 && (number % 5) == 1
     "FizzBuzz"
   else
 end
